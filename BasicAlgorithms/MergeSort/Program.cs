38328f8b4446944228d1c4802d9eb6c903196d9d
stevenjc721 <stevenjc721@gmail.com>
2016-07-21 15:39:09 -0400

    QuickSort & MergeSort

---
+++ BasicAlgorithms/MergeSort/Program.cs
@@ -0,0 +1,139 @@
+﻿/*
+ * @author Steven J Carver
+ * 
+ * Example of Merge Sort.
+ * 
+ * Example Input:
+ *         23, 3, 2, 34, 6
+ *      
+ *       23, 3, 2        34, 6
+ *      
+ *     23, 3    2       34    6
+ *     
+ *    23   3
+ *      
+ *         23, 3, 2     34, 6
+ *         
+ *           34, 23, 6, 3, 2
+ * Output:
+ * 34, 23, 6, 3, 2
+ * 
+ * Merge Sort:
+ * Stable
+ * Based on Divide and Conquer
+ * Asymptotic complexity O(n * log n)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Unsorted Array of ints
+            int[] iArray = new int[22] { 4, 13, 11, 2, 7, 5, 14, 3, 18, 12, 1, 19, 6, 9, 17, 20, 10, 15, 8, 16, 0, -1 };
+
+            mSort(iArray); //Merge Array
+
+            //Print Merge Array
+            foreach (var x in iArray){
+
+                Console.Out.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+
+        //Initial Step: Get values for recursion
+        //I prefer to seperate base call from recursion, this is a personal preferences and not needed.
+        static void mSort(int[] array){
+
+            int[] aux = new int[array.Length];
+            int left = 0;
+
+            int right = array.Length - 1;
+
+            mSort(array, aux, left, right);
+
+        }
+
+        //Quick Sort
+        public static void mSort(int[] array, int[] aux, int left, int right){
+
+            //Base case
+            if (left == right){
+
+                return;
+            }
+            //identify current middle
+            int middle = (left + right) / 2;
+            
+            //Recursive call to sort left side
+            mSort(array, aux, left, middle);
+
+            //Recursive call to sort right side
+            mSort(array, aux, middle + 1, right);
+
+            //Merging the left and right side together
+            merge(array, aux, left, right);
+
+            //Reorganizing our input array to match computed order
+            for (int i = left; i <= right; i++){
+
+                array[i] = aux[i];
+            }
+        }
+
+        private static void merge(int[] array, int[] aux, int left, int right){
+
+            //Current middle value
+            int middle = (left + right)/2;
+            
+            //Copy of left index to be manipulated
+            int lIndex = left;
+
+            //Copy of right index to be manipulated
+            int rIndex = middle + 1;
+
+            //Current aux array index
+            int aIndex = left;
+
+            //While our left and right indexes are within their appropriate subarrays continue
+            while (lIndex <= middle && rIndex <= right){
+
+                //If value at left index is greater/equal to our value at right index set aux value for current aux Index and increase current left index
+                if (array[lIndex] >= array[rIndex]){
+
+                    aux[aIndex] = array[lIndex++];
+                }
+                //If previous case not true we step to our next value on the right subarray
+                else{
+
+                    aux[aIndex] = array[rIndex++];
+                }
+                //Step forward in our aux array for next value placement
+                aIndex++;
+            }
+
+            //Case when for values not caught by previous cases
+            while (lIndex <= middle){
+                
+                //If left values remain add to aux at the current aux Index and step forward
+                aux[aIndex] = array[lIndex++];
+                aIndex++;
+            }
+            //Case when for values not caught by previous cases
+            while (rIndex <= right){
+                //If right ralues remain, add to aux at the current aux Index and step forward 
+                aux[aIndex] = array[rIndex++];
+                aIndex++;
+            }
+        }
+    }
+}
---
+++ BasicAlgorithms/QuickSort/Program.cs
@@ -0,0 +1,102 @@
+﻿/*
+ * @author Steven J Carver
+ * 
+ * Example of Quick Sort.
+ * 
+ * Example Input:
+ *         23, 3, 2, 34, 6
+ *          23(pivot)
+ *  34                  3, 2, 6
+ *  34(base case)         3(pivot)
+ *               6(base case)    2(base case)
+ * Output:
+ * 34, 23, 6, 3, 2
+ * 
+ * Quick Sort:
+ * Unstable
+ * Based on Divide and Conquer
+ * Asymptotic complexity O(n^2)
+ * Expected complexity is O(n * log n)
+ * Typically preforms better than Heap and Merge Sort
+ */
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            //Unsorted Array of ints
+            int[] iArray = new int[22] { 4, 13, 11, 2, 7, 5, 14, 3, 18, 12, 1, 19, 6, 9, 17, 20, 10, 15, 8, 16, 0, -1 };
+
+            qSort(iArray); //Quick Array
+
+            //Print Quick Array
+            foreach (var x in iArray){
+                
+                Console.Out.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+
+        //Initial Step: Get values for recursion
+        //I prefer to seperate base call from recursion, this is a personal preferences and not needed.
+        static void qSort(int[] array){
+
+            int left = 0;
+
+            int right = array.Length - 1;
+
+            qSort(array, left, right);
+
+        }
+
+        //Quick Sort
+        //We first pick a random element and this becomes our pivot.
+        //From the select pivot we reorganize the array so that all the elements that have a greater value than our pivot are placed to the left
+        //of our pivot. All the elements with a lower value are placed after the pivot, or on the right.
+        //For this example we will use a recursive approach, an iterative approach is also possible.
+        //Our program will now make a recursive call and repeat the procedure until our subproblem size = 1.
+        //When we reach this case our problem can we solved trivially. If we have one element it is already sorted.
+        //Sorted in descending order.
+        public static void qSort(int[] array, int left, int right){
+
+            if (left < right){
+
+                int pivot = left;
+                
+                //We examine elements to the right of our pivot. Because in this case our pivot is our first element
+                for (int i = left + 1; i < right; i++){
+
+                    //If the current element is greater than our pivot, we push the element to the left
+                    if (array[i] > array[left]){
+
+                        swap(array, i, ++pivot);
+                    }
+                }
+                //Place our pivot element on the greatest index on the left
+                swap(array, left, pivot);
+
+                //Recursive call to sort elements to the left of our pivot
+                qSort(array, left, pivot);
+                //Recursive call to sort elements to the right of our pivot
+                qSort(array, pivot + 1, right);
+            }
+        }
+
+        //Simple swap for element at index left and index right
+        private static void swap(int[] array, int left, int right){
+
+            int temp = array[right];
+            array[right] = array[left];
+            array[left] = temp;
+        }
+    }
+}
